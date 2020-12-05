887fec38cd88913fc14963f8d147f4024da3b572
Tamir <1tamir198@gmail.com>
2020-12-05 16:33:56 +0200

    Adding firebase connection
    IN addition, cloud firestore connection was added

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -10,6 +11,19 @@ class ChatScreen extends StatelessWidget {
                 padding: EdgeInsets.all(8),
                 child: Text('Dummy data'),
               )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          //This is a stream that listen to changes inside cloud firestore
+          Firestore.instance.collection('Chats/12dS8Qv2FgFDhzsiaavq/messages').snapshots().listen((event) {
+            print(event.documents[0]['Text']);
+
+            event.documents.forEach((element) {
+              print(element['Text']);
+            });
+          });
+        }
+      ),
     );
   }
 }
